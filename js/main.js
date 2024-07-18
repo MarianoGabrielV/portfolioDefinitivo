e8df8f588c37d5366bf593913c05c8bcf9bd8691
MarianoVergniaud <mariano.unla@gmail.com>
2024-07-18 10:05:53 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+// main.js
+
+document.addEventListener('DOMContentLoaded', function(){
+    var typed = new Typed('.typed-text', {
+      strings: ["Tecnico en Programacion", "Docente Informatico", "Freelancer", "Apasionado por la programación"], // Arreglo de cadenas de texto a mostrar
+      typeSpeed: 50, // Velocidad de escritura en milisegundos
+      backSpeed: 30, // Velocidad de borrado en milisegundos
+      loop: true // Repetir el efecto
+    });
+  });
+  
